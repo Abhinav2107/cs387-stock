e93dfb465284481a464d2b8c53f5595c55effc7a
pandabear93 <apande15@asu.edu>
2014-11-23 14:27:42 +0530

    Create CompanyStock.java

---
+++ src/CompanyStock.java
@@ -0,0 +1,16 @@
+public class CompanyStock
+{
+	public String symbol;
+	public String company;
+	public float ltp;
+       
+    public CompanyStock(String sym, String com, float val)
+    {
+        symbol = sym;
+        company = com;
+        ltp = val;
+    }
+    
+    public void print()
+    {	System.out.println(symbol + " " + company + " " + ltp);	}
+}
